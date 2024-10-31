f5910d394134f881718f8cddadac51b8384572f6
wellyhsu <120746227+wellyhsu@users.noreply.github.com>
2024-10-31 22:45:39 +0800

    Add files via upload

--- HandMesh/count_parameters.py
+++ HandMesh/count_parameters.py
@@ -12,11 +12,9 @@ from conv.dsconv import DSConv
 from mobrecon.build import MODEL_REGISTRY
 from mobrecon.main import setup
 from options.cfg_options import CFGOptions
-from countParams import clever_format, profile
+from thop import clever_format, profile
 from torchinfo import summary
 import pandas as pd
-import re
-import ast
 
 #from models.imagenet import mobilenetv2
 
@@ -26,7 +24,7 @@ import ast
 @MODEL_REGISTRY.register()
 # Replace model here
 class MobRecon_DS(nn.Module):
-    def __init__(self, cfg):
+    def __init__(self, cfg, control=None):
         """Init a MobRecon-DenseStack model
 
         Args:
@@ -34,8 +32,9 @@ class MobRecon_DS(nn.Module):
         """
         super(MobRecon_DS, self).__init__()
         self.cfg = cfg
+        self.control = control  # 將 control 儲存為實例變數
         self.backbone = DenseStack_Backnone(latent_size=cfg.MODEL.LATENT_SIZE,
-                                            kpts_num=cfg.MODEL.KPTS_NUM)
+                                            kpts_num=cfg.MODEL.KPTS_NUM, control=self.control)
         cur_dir = os.path.dirname(os.path.realpath(__file__))
         template_fp = os.path.join(cur_dir, 'template/template.ply')
         transform_fp = os.path.join(cur_dir, 'template', 'transform.pkl')
@@ -77,14 +76,8 @@ args.config_file = 'mobrecon/configs/mobrecon_ds.yml'
 cfg = setup(args)
 
 # 定義模型並傳遞 cfg
-model = MobRecon_DS(cfg)
-
- # 完整保存model架構和參數
-torch.save(model, 'MobRecon_DS_Densestack.pth')
-
-# 載入model
-model_path = 'MobRecon_DS_Densestack.pth'
-model = torch.load(model_path)
+# 選擇要計算的model版本: Densestack, mobilenet_v3
+model = MobRecon_DS(cfg, control='Densestack')
 
 # 修改 input 的大小，符合模型的輸入
 input = torch.randn(1, 6, 128, 128)
@@ -110,8 +103,23 @@ output_max_tensor_info = ""
 
 layers = model_summary.summary_list
 
+handle_collection=[]
+
 for layer in layers:
-    param_count = layer.trainable_params  # 獲取參數量，使用 num_params
+    inner_layers = layer.inner_layers
+    layer_ids = [layer.layer_id]
+    
+    if layer.layer_id in handle_collection:
+        continue
+    handle_collection.append(layer.layer_id)
+
+    if layer.is_leaf_layer:
+        param_count = layer.trainable_params  # 獲取參數量，使用 trainable_params
+        if param_count > max_params:
+            max_params = param_count
+            max_params_layer_name = layer.class_name
+    else:
+        param_count=0
     input_shape = layer.input_size  # 獲取輸入大小
     output_shape = layer.output_size  # 獲取輸出大小         
 
@@ -131,16 +139,16 @@ for layer in layers:
 
     summary_data.append({
         'Layer': layer.class_name,
-        'Depth' : str(layer.depth)+"-"+str(layer.depth_index),
         'Input Size': input_shape,
         'Output Size': output_shape,
         'Input Tensor': input_tensor_size,
         'Output Tensor': output_tensor_size,
         'Kernel Size': layer.kernel_size,
+        'Params': param_count
     })
-
+    
 # 計算 FLOPs 和參數
-flops, params, max_params, max_params_layer_name = profile(model, inputs=(input,), max_params=max_params, max_params_layer_name=max_params_layer_name)
+flops, params = profile(model, inputs=(input,))
 flops, params = clever_format([flops, params], "%.3f")
 
 input_max_tensor_size = clever_format([input_max_tensor_size], "%.3f")
